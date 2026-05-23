c7a17f0facb28fd5423d40cae0ccb99cbc239cc0
José Manuel Leiva <leiva.ruano@gmail.com>
2026-05-23 17:41:48 -0600

    Revert "feat: ejemplo switch"
    This reverts commit ea447d0307cfa0e3ec57e79b81a5c1b9c749c3de.

--- ejemplo.cpp
+++ ejemplo.cpp
@@ -2,31 +2,12 @@
 using namespace std;
 int main()
 {
-    int a;
-    cout << "Ejemplo Switch C++" << endl;
-    cout << "Ingresa un numero del 1 al 5: ";
-    cin >> a;
+    int a = 10;
+    cout << "Ejemplo if con C++" << endl;
 
-    switch (a)
+    if (a > 5)
     {
-    case 1:
-        cout << "Ingresaste el numero 1" << endl;
-        break;
-    case 2:
-        cout << "Ingresaste el numero 2" << endl;
-        break;
-    case 3:
-        cout << "Ingresaste el numero 3" << endl;
-        break;
-    case 4:
-        cout << "Ingresaste el numero 4" << endl;
-        break;
-    case 5:
-        cout << "Ingresaste el numero 5" << endl;
-        break;
-    default:
-        cout << "Ingresaste un numero fuera del rango" << endl;
-        break;
+        cout << "a es mayor que 5" << endl;
     }
 
     return 0;
