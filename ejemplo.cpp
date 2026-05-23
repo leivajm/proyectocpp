4a39c6f1d0cb88d054373460903ce60a35a43ecd
José Manuel Leiva <leiva.ruano@gmail.com>
2026-05-23 17:27:03 -0600

    feat: ejemplo switch

--- ejemplo.cpp
+++ ejemplo.cpp
@@ -2,12 +2,31 @@
 using namespace std;
 int main()
 {
-    int a = 10;
-    cout << "Ejemplo if con C++" << endl;
+    int a;
+    cout << "Ejemplo Switch C++" << endl;
+    cout << "Ingresa un numero del 1 al 5: ";
+    cin >> a;
 
-    if (a > 5)
+    switch (a)
     {
-        cout << "a es mayor que 5" << endl;
+    case 1:
+        cout << "Ingresaste el numero 1" << endl;
+        break;
+    case 2:
+        cout << "Ingresaste el numero 2" << endl;
+        break;
+    case 3:
+        cout << "Ingresaste el numero 3" << endl;
+        break;
+    case 4:
+        cout << "Ingresaste el numero 4" << endl;
+        break;
+    case 5:
+        cout << "Ingresaste el numero 5" << endl;
+        break;
+    default:
+        cout << "Ingresaste un numero fuera del rango" << endl;
+        break;
     }
 
     return 0;
