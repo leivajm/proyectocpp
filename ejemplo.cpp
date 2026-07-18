b029e43d96e3a6c53b5bde6098a73fc0ac138c62
José Manuel Leiva <leiva.ruano@gmail.com>
2026-07-18 12:07:12 -0600

    feat: ejemplo for

--- ejemplo.cpp
+++ ejemplo.cpp
@@ -3,30 +3,10 @@ using namespace std;
 int main()
 {
     int a;
-    cout << "Ejemplo Switch C++" << endl;
-    cout << "Ingresa un numero del 1 al 5: ";
-    cin >> a;
-
-    switch (a)
+    cout << "Ejemplo For C++" << endl;
+    for (a = 1; a <= 10; a++)
     {
-    case 1:
-        cout << "Ingresaste el numero 1" << endl;
-        break;
-    case 2:
-        cout << "Ingresaste el numero 2" << endl;
-        break;
-    case 3:
-        cout << "Ingresaste el numero 3" << endl;
-        break;
-    case 4:
-        cout << "Ingresaste el numero 4" << endl;
-        break;
-    case 5:
-        cout << "Ingresaste el numero 5" << endl;
-        break;
-    default:
-        cout << "Ingresaste un numero fuera del rango" << endl;
-        break;
+        cout << "El valor de a es: " << a << endl;
     }
 
     return 0;
